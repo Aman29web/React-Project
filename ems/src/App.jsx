8446e144c7bfaadc44cdff47824068bb6c1692b5
Aman29web <amansinghvns29@gmail.com>
2024-11-07 14:41:51 +0530

    Done

---
+++ ems/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Login from './componenets/Auth/Login'
+
+const App = () => {
+  return (
+    <>
+     
+    <Login/>
+
+    
+    </>
+    
+  )
+}
+
+export default App
---
+++ ems/src/componenets/Auth/Login.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    
+    const submitHandler=(e)=>{
+        e.preventDefault()
+         console.log("email is",email)
+         console.log("password is",password)
+         setEmail("")
+         setPassword("")
+    }
+    
+
+
+
+  return (
+    <div className='flex h-screen w-screen items-center justify-center'>
+        <div className='border-2 border-emerald-600 p-20 rounded-xl'>
+            <form 
+                  onSubmit={(e)=>{
+                    submitHandler(e)
+                  }}
+            
+            className='flex flex-col items-center justify-center'>
+                <input 
+                value={email}
+                 onChange={(e)=>{
+                    setEmail(e.target.value)
+                    
+                }}
+
+                required type="email" placeholder='Enter your email' className='border-2 border-emerald-600 rounded-full py-4 px-5 text-xl text-white outline-none bg-transparent placeholder:text-gray-400'/>
+                <input 
+                value={password}
+                onChange={(e)=>{
+                        setPassword(e.target.value)
+                }}
+                
+                required type="password" placeholder='Enter password' className='mt-4 border-2 border-emerald-600 rounded-full py-4 px-5 text-xl text-white outline-none bg-transparent placeholder:text-gray-400'/>
+                <button className='border-none mt-5 border-2 rounded-full py-4 px-5 text-xl text-white outline-none bg-emerald-600 placeholder:text-white'>Log in</button>
+            </form>
+        </div>
+       
+      
+    </div>
+  )
+}
+
+export default Login
